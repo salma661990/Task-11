ea615f58126c0e6ae3e5f7e06d78110dd93f1bd4
salma661990 <66919987+salma661990@users.noreply.github.com>
2020-07-24 01:58:00 +0300

    Add files via upload

---
+++ Dc_Motor_With_two_Relay.ino
@@ -0,0 +1,35 @@
+void setup()
+
+{
+
+  pinMode(13, OUTPUT);
+
+  pinMode(12, OUTPUT);
+
+}
+
+
+
+void loop()
+
+{
+
+
+
+    digitalWrite(13, HIGH);
+
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(13, LOW);
+
+  delay(1000); // Wait for 1000 millisecond(s)
+
+   digitalWrite(12, LOW);
+
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(12, HIGH);
+
+  delay(1000); // Wait for 1000 millisecond(s)
+
+}
